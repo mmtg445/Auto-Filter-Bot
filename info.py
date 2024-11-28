6f5034c57af5dd6d198878326f9cf825a846878c
mmtg445 <mmtg445@gmail.com>
2024-11-28 22:22:14 +0600

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ else:
     LOG_CHANNEL = int(LOG_CHANNEL)
 
 # support group
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-4574811926')
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1002470486707')
 if len(SUPPORT_GROUP) == 0:
     print('Error - SUPPORT_GROUP is missing, exiting now')
     exit()
